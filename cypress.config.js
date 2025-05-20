6cf955c6f8ce1619a723e30f068868036f11969b
Puneet Kaur Hundal <puneetkh.qa@gmail.com>
2025-05-20 12:31:56 -0700

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video : true,
    reporter: 'cypress-mochawesome-reporter',
+     reporterOptions: {
+    reportDir: 'cypress/reports/html',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     baseUrl : 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
