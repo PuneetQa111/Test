7cb13dbf5c2d4cbddefce2cccc6942c40175bf9c
Puneet Kaur Hundal <puneetkh.qa@gmail.com>
2025-05-30 13:07:16 -0700

    API automation using cypress

---
+++ cypress/e2e/tests/getUsersinfo.cy.js
@@ -0,0 +1,32 @@
+describe('API automation', () => {
+  it('Get Users info', () => {
+    
+    cy.log('')
+   cy.request({
+method: 'GET',
+url : 'https://gorest.co.in/public/v2/users' ,
+Headers : {Authorization :"Bearer e8929b0e4783ae39264b36c246e5ec6c3bd7f60022494b6c3dbe61eb8addbea7"},
+
+   })
+   .then((response)=>{
+    cy.log(JSON.stringify(response)) //print the body or response of the request , convert object response into json
+    expect(response.status).to.equal(200)
+   })
+
+   
+ 
+  })
+  it('Get single user info', () => {
+
+
+    cy.request({
+        method:'GET',
+        url: 'https://gorest.co.in/public/v2/users/7439527',
+        Headers : {Authorization :"Bearer e8929b0e4783ae39264b36c246e5ec6c3bd7f60022494b6c3dbe61eb8addbea7"},
+    })
+    .then((response)=>{
+         expect(response.status).to.equal(200)
+         expect(response.body.id).to.equal(7439527)
+    })
+  })
+})
