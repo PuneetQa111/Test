867d89292113b246cd7e369c042edf1e75caa2c2
Puneet Kaur Hundal <puneetkh.qa@gmail.com>
2025-05-30 13:28:29 -0700

    Added negative scenario use cases

--- cypress/e2e/tests/getUsersinfo.cy.js
+++ cypress/e2e/tests/getUsersinfo.cy.js
@@ -1,24 +1,19 @@
 describe('API automation', () => {
   it('Get Users info', () => {
-    
-    cy.log('')
-   cy.request({
-method: 'GET',
-url : 'https://gorest.co.in/public/v2/users' ,
-Headers : {Authorization :"Bearer e8929b0e4783ae39264b36c246e5ec6c3bd7f60022494b6c3dbe61eb8addbea7"},
+     cy.request({
+     method: 'GET',
+     url : 'https://gorest.co.in/public/v2/users' ,
+     Headers : {Authorization :"Bearer e8929b0e4783ae39264b36c246e5ec6c3bd7f60022494b6c3dbe61eb8addbea7"},
 
    })
    .then((response)=>{
-    cy.log(JSON.stringify(response)) //print the body or response of the request , convert object response into json
-    expect(response.status).to.equal(200)
+       cy.log(JSON.stringify(response)) //print the body or response of the request , convert object response into json
+       expect(response.status).to.equal(200)
    })
-
-   
- 
   })
+
+
   it('Get single user info', () => {
-
-
     cy.request({
         method:'GET',
         url: 'https://gorest.co.in/public/v2/users/7439527',
@@ -27,6 +22,33 @@ Headers : {Authorization :"Bearer e8929b0e4783ae39264b36c246e5ec6c3bd7f60022494b
     .then((response)=>{
          expect(response.status).to.equal(200)
          expect(response.body.id).to.equal(7439527)
+    })
+  })
+
+  it('Invalid endpoint - negative use case', () => {
+    cy.request({
+        method:'GET',
+        url: 'https://gorest.co.in/public/v2/user',
+        Headers : {Authorization :"Bearer e8929b0e4783ae39264b36c246e5ec6c3bd7f60022494b6c3dbe61eb8addbea7"},
+        failOnStatusCode: false
+    })
+    .then((response)=>{
+         expect(response.status).to.equal(404)
+         
+    })
+  })
+
+  
+  it('Invalid user - negative use case', () => {
+    cy.request({
+        method:'GET',
+        url: 'https://gorest.co.in/public/v2/users/123456',
+        Headers : {Authorization :"Bearer e8929b0e4783ae39264b36c246e5ec6c3bd7f60022494b6c3dbe61eb8addbea7"},
+        failOnStatusCode: false
+    })
+    .then((response)=>{
+         expect(response.status).to.equal(404)
+         
     })
   })
 })
